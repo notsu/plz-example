8080b00efde67ae6ba5a06c9bf5870c1f33ffc6f
Pichet Itngam <chilly.notsu@gmail.com>
2018-12-31 16:48:11 +0700

    chore: initiate project

---
+++ services/post/go.mod
@@ -0,0 +1,1 @@
+module github.com/notsu/plz-example/services/post
---
+++ services/user/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(User())
+}
+
+// User returns string of "User"
+func User() string {
+	return "User"
+}
